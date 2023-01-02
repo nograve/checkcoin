fb18e5cbba242548a2f691dc390ab61d056fc48d
Ihor Zakharov <nograveoff@gmail.com>
2023-01-02 22:08:26 +0200

    added home page

---
+++ lib/check_coin_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CheckCoinApp extends StatelessWidget {
+  const CheckCoinApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CheckCoin App',
+      home: Container(),
+    );
+  }
+}
