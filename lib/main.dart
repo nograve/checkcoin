487fc37a8d9af770e69571754f32c29c81aa4a03
Ihor Zakharov <nograveoff@gmail.com>
2023-01-03 13:12:40 +0200

    added loaded config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'check_coin_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
   runApp(const CheckCoinApp());
+}
+
+Future<void> loadConfig() async {
+  String configContent = await rootBundle.loadString('assets/config/main.json');
+  Map configData = jsonDecode(configContent);
+  print(configData);
 }
