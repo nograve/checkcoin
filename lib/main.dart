f228e875eabb13a508a41e4f54246813ab9a47b6
Ihor Zakharov <nograveoff@gmail.com>
2023-01-03 13:41:51 +0200

    registered Singleton for AppConfig

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
+import 'package:checkcoin/models/app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
 import 'check_coin_app.dart';
 
@@ -13,5 +15,9 @@ void main() async {
 Future<void> loadConfig() async {
   String configContent = await rootBundle.loadString('assets/config/main.json');
   Map configData = jsonDecode(configContent);
-  print(configData);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: configData['COIN_API_BASE_URL'],
+    ),
+  );
 }
