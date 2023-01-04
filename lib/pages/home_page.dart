2e6e0bf775fb9e0907c1dc96101cb79906a236d9
Ihor Zakharov <nograveoff@gmail.com>
2023-01-04 11:29:52 +0200

    added DropDownMenu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Widget _selectedCoinDropdown() {
+    List<String> coins = ['bitcoin'];
+    List<DropdownMenuItem<String>> items = coins
+        .map(
+          (e) => DropdownMenuItem(
+            value: e,
+            child: Text(
+              e,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        )
+        .toList();
+    return DropdownButton(
+      value: coins.first,
+      items: items,
+      onChanged: (value) {},
+      dropdownColor: const Color.fromRGBO(83, 88, 206, 1.0),
+      iconSize: 30,
+      icon: const Icon(
+        Icons.arrow_drop_down,
+        color: Colors.white,
+      ),
+      underline: Container(),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final deviceHeight = MediaQuery.of(context).size.height;
+    final deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _selectedCoinDropdown(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
