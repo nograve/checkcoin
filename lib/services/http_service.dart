298b809523627bb822952c7538794647abfc3f4b
Ihor Zakharov <nograveoff@gmail.com>
2023-01-15 19:19:17 +0200

    making fields private

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -4,20 +4,20 @@ import 'package:get_it/get_it.dart';
 import 'package:checkcoin/models/app_config.dart';
 
 class HTTPService {
-  final Dio dio = Dio();
+  final Dio _dio = Dio();
 
-  AppConfig? appConfig;
-  String? baseUrl;
+  AppConfig? _appConfig;
+  String? _baseUrl;
 
   HTTPService() {
-    appConfig = GetIt.instance.get<AppConfig>();
-    baseUrl = appConfig!.coinApiBaseUrl;
+    _appConfig = GetIt.instance.get<AppConfig>();
+    _baseUrl = _appConfig!.coinApiBaseUrl;
   }
 
   Future<Response?> get(String path) async {
     try {
-      String url = '$baseUrl$path';
-      Response response = await dio.get(url);
+      String url = '$_baseUrl$path';
+      Response response = await _dio.get(url);
       return response;
     } catch (e) {
       return null;
